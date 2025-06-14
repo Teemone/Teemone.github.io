81735ca2be79a1dc792dc3569782691c83a7699e
Timothy <fynx626@gmail.com>
2025-06-14 18:02:05 +0100

    Post initial commit

---
+++ generate_portfolio.py
@@ -0,0 +1,47 @@
+# PURPOSE IS TO LOAD DATA AND GENERATE TEMPLATES FROM THE DATA
+# ADDITIONAL PROCESSING CAN BE DONE BEFORE GENERATING THE TEMPLATES
+
+import json
+from datetime import UTC, datetime
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
+
+# Load JSON data
+with Path("portfolio_swe.json").open(encoding="utf-8") as f:
+    data = json.load(f)
+    
+# Add any extra context if needed
+data["current_year"] = datetime.now(tz=UTC).year
+
+
+if "social_links" in data:
+    for link in data["social_links"]:
+        if link.get("svg_path"):
+            with Path(link["svg_path"]).open(encoding="utf-8") as svg_file:
+                link["svg_data"] = svg_file.read()
+
+      
+
+# Set up Jinja environment
+env = Environment(loader=FileSystemLoader("."), autoescape=True)
+index_template = env.get_template("index_template.html")
+resume_template = env.get_template("resume_template.html")
+
+# Render the temmplate with the data
+html_output = index_template.render(**data)
+resume_output = resume_template.render(**data)
+
+# This is equivalent to...
+# html_output = index_template.render(name=data["name"], label=data["label"]...)
+# resume_output = resume_template.render(name=data["name"], label=data["label"]...)
+# ** is used to unpack the collection / iterable
+
+# Write the output to an HTML file
+with Path("index.html").open("w", encoding="utf-8") as f:
+    f.write(html_output)
+    
+with Path("resume.html").open("w", encoding="utf-8") as f:
+    f.write(resume_output)
+    
+print("Tasks completed successfully")
